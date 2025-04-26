d2c9a63eb7b697689ce1ae7fb582e0da777412e7
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2025-04-26 22:39:53 +0200

    feat(polygraph): solve script

--- speedrun/polygraph/solve.py
+++ speedrun/polygraph/solve.py
@@ -1,62 +1,59 @@
 from z3 import *
+from program import PROGRAM
+import hashlib
+from pwn import *
 
-with open("src/polygraph.exe", "rb") as f :
-    key = f.read()
-    start = key.find(b'\x16\x00\x00\x16\x01\x01\x16\x02\x02\x16\x03\x03\x16')
-    key = key[start:start + 774 * 3]
-
-i = 773
-eip = 1
-
-memory = [BitVec(f'mem_{i}', 32) for i in range(5)]  # 32-bit symbolic variables
-input = [BitVec(f'input_{i}', 8) for i in range(17)]
+memory = [BitVec(0, 32) for i in range(10)]
+input_bytes = [BitVec(f'input_{i}', 8) for i in range(16)]
 
 solver = Solver()
 
-while True:
-    INSTR = key[eip - 1]
-    PTR = key[eip]
-    PARAM = key[eip + 1]
+eip = 1
+for eip in range(1, len(PROGRAM), 3):
+    INSTR = PROGRAM[eip - 1]
+    PTR = PROGRAM[eip]
+    PARAM = PROGRAM[eip + 1]
 
-    if INSTR == 0xb :
-        R1 = memory[PARAM]
-        memory[PTR] += R1
-        print(f"MEM[{PTR}] += R1 ({R1})")
-    elif INSTR == 0x16 :
-        memory[PTR] -= memory[PARAM]
-        print(f"MEM[{PTR}] -= MEM[{PARAM}]")
-    elif INSTR == 0x2c :
-        memory[PTR] = LShR(memory[PTR], (PARAM & 0b11111))
-        print(f"MEM[{PTR}] = MEM[{PTR}] << {PARAM & 0b11111}")
-    elif INSTR == 0x37 :
-        memory[PTR] = memory[PTR] >> (PARAM & 0b11111)
-        print(f"MEM[{PTR}] = MEM[{PTR}] >> {PARAM & 0b11111}")
-    elif INSTR == 0x42 :
-        memory[PTR] *=  memory[PARAM]
-        print(f"MEM[{PTR}] *= MEM[{PARAM}]")
-    elif INSTR == 0x4d :
+    if INSTR == 0xb:
+        memory[PTR] = memory[PTR] + memory[PARAM]
+    elif INSTR == 0x16:
+        memory[PTR] = memory[PTR] - memory[PARAM]
+    elif INSTR == 0x2c:
+        memory[PTR] = memory[PTR] << (PARAM & 0x1f)
+    elif INSTR == 0x37:
+        memory[PTR] = LShR(memory[PTR], (PARAM & 0x1f))
+    elif INSTR == 0x42:
+        memory[PTR] = memory[PTR] * memory[PARAM]
+    elif INSTR == 0x4d:
         memory[PTR] = memory[PTR] | memory[PARAM]
-        print(f"MEM[{PTR}] |= MEM[{PARAM}]")
-    elif INSTR == 0x58 :
-        memory[PTR] = ZeroExt(24, input[PARAM])
-        print(f"MEM[{PTR}] = INPUT[{PARAM}]")
-    elif INSTR == 0x63 :
-        R1 = PARAM
-        memory[PTR] += R1
-        print(f"MEM[{PTR}] += {PARAM}")
-    else :
-        print("BAD INSTRUCTION")
+    elif INSTR == 0x58:
+        memory[PTR] = ZeroExt(24, input_bytes[PARAM])
+    elif INSTR == 0x63:
+        memory[PTR] = memory[PTR] + PARAM
+    else:
+        print(f"BAD INSTRUCTION at {eip-1}: {hex(INSTR)}")
         break
 
-    print(memory)
-    i -= 1
-    eip += 3
-    if i == 0 :
-        break
+solver.add(memory[0] == 0)
 
 if solver.check() == sat:
     model = solver.model()
-    solution = bytes([model[byte].as_long() for byte in input])
-    print(f"Found valid input: {solution}")
+    print(model)
+    solution = bytes([model[input_bytes[i]].as_long() for i in range(16)])
+    print(f"{solution}")
 else:
-    print("No solution found.")
+    print("no solution")
+    exit()
+
+import hashlib
+from pwn import *
+
+context.log_level = "debug"
+
+with process("./src/polygraph.exe") as r :
+    r.sendlineafter(">>>", solution)
+    r.recvline()
+    r.recvline()
+h = hashlib.sha512(data).digest()
+flag = "FCSC{" + ''.join(f"{b:02x}" for b in h) + "}"
+print(flag)
